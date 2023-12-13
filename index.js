e2bc9b3b23ae8a25ea32ba7d7e4aaf8c847e5823
Shamim Ahammad <shamimfstack@gmail.com>
2023-12-13 08:57:37 +0600

    web token generation added

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 require('dotenv').config();
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+      'http://localhost:5173',
+      // 'https://concept-1-bbffd.web.app',
+      // 'https://concept-1-bbffd.firebaseapp.com'
+
+  ],
+  credentials: true
+}))
 app.use(express.json())
 
 
@@ -31,8 +41,42 @@ async function run() {
     const userCollection = client.db("schoolDB").collection("users");
     const studentCollection = client.db("schoolDB").collection("students");
 
+    // jwt related api
+    // create token
+    app.post('/jwt', async(req, res) => {
+      const user = req.body;
+      console.log(user);
+      console.log('user for token', user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, 
+          {expiresIn: '1h'});
 
-    
+        // res.cookie('token', token, {
+        //     httpOnly: true,
+        //     secure: process.env.NODE_ENV === 'production',
+        //     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        //     })
+        //   .send({status: 'success'})
+     
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: false,
+        sameSite: 'none'
+      })
+      .send({status: "true"})
+  })
+
+    // user related api
+    app.post('/users', async(req, res) => {
+        const user = req.body;
+
+        const query = { email: user.email }
+        const existingUser = await userCollection.findOne(query)
+        if(existingUser) {
+          return res.send({ message: "User already exists", insertedId: null})
+        }
+        const result = await userCollection.insertOne(user)
+        res.send(result);
+    })
 
 
 
