6bf6b4e10ea22e5246e8dcfc65e39b5bc5c77622
Shamim Ahammad <shamimfstack@gmail.com>
2023-12-13 06:51:40 +0600

    environment variable added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 require('dotenv').config();
 const cors = require("cors");
@@ -11,6 +11,40 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.24pgglg.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const userCollection = client.db("schoolDB").collection("users");
+    const studentCollection = client.db("schoolDB").collection("students");
+
+
+    
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
